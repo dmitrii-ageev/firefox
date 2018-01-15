eb5acc8741be5954822e323429d62b3e98550853
Dmitrii Ageev <dageev@gmail.com>
2018-01-15 14:06:10 +1300

    Added shell-script

---
+++ firefox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker run -d --rm --name firefox -h docker \
+       --net="host" \
+       -e DISPLAY=$DISPLAY \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v /etc/machine-id:/etc/machine-id \
+       -v /dev/snd:/dev/snd \
+       -v $XDG_RUNTIME_DIR/pulse:$XDG_RUNTIME_DIR/pulse \
+       -v $HOME/.cache:/home/developer/.cache \
+       -v $HOME/.mozilla:/home/developer/.mozilla \
+       -v $HOME/Downloads:/home/developer/Downlods \
+       dmitriiageev/firefox
